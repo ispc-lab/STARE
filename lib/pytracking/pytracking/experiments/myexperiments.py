245d6705da4fea5ed8018b5d9dd8c494ffebf6e6
Runze Zhang <101967641+NoRealBlank@users.noreply.github.com>
2025-07-03 21:29:51 +0800

    Update myexperiments.py

--- lib/pytracking/pytracking/experiments/myexperiments.py
+++ lib/pytracking/pytracking/experiments/myexperiments.py
@@ -1,6 +1,21 @@
 from pytracking.evaluation import Tracker, get_dataset, trackerlist, load_stream_setting
 
 
+# Note:
+# range(1) means run_id=0, the tracking results will
+# be saved in pytracking/output/tracking_results/{tracker_name}/{tracker_params}_{run_id}/
+# e.g. pytracking/output/tracking_results/atom/default_000/
+# if you set run_id=None, the tracking results will
+# be saved in pytracking/output/tracking_results/{tracker_name}/{tracker_params}/
+# e.g. pytracking/output/tracking_results/atom/default/
+
+def fast_test_offline():
+    trackers =  trackerlist('atom', 'default', range(1)) + \
+                trackerlist('dimp', 'dimp18', range(1)) + \
+                trackerlist('kys', 'default', range(1))
+    dataset = get_dataset('esot_20_50')
+    return trackers, dataset
+    
 def atom_nfs_uav():
     # Run three runs of ATOM on NFS and UAV datasets
     trackers = trackerlist('atom', 'default', range(3))
@@ -45,13 +60,6 @@ def default_offline():
                 trackerlist('keep_track','default',range(1)) +\
                 trackerlist('dimp', 'prdimp18', range(1))
     dataset = get_dataset('esot500')
-    return trackers, dataset
-
-def fast_test_offline():
-    trackers =  trackerlist('atom', 'default', range(1)) + \
-                trackerlist('dimp', 'dimp18', range(1)) + \
-                trackerlist('kys', 'default', range(1))
-    dataset = get_dataset('esot_20_50')
     return trackers, dataset
 
 def esot500_offline():
