fd0a01a1b75db2766c7bda79b51b44563c033d06
Runze Zhang <101967641+NoRealBlank@users.noreply.github.com>
2025-07-03 21:27:46 +0800

    Update exp_streaming.py

--- lib/pytracking/pytracking/experiments/exp_streaming.py
+++ lib/pytracking/pytracking/experiments/exp_streaming.py
@@ -1,5 +1,13 @@
 from pytracking.evaluation import Tracker, get_dataset, trackerlist, load_stream_setting
 
+# Note:
+# Currently, default run_id is None and stream_setting_id=0, the tracking results will eventually
+# be saved in pytracking/output/tracking_results_rt_final/{tracker_name}/{tracker_params}/{stream_setting_id}/
+# e.g. pytracking/output/tracking_results_rt_final/atom/default/0/
+# if you set run_id=0, the tracking results will
+# be saved in pytracking/output/tracking_results_rt_final/{tracker_name}/{tracker_params}_{run_id}/{stream_setting_id}/
+# e.g. pytracking/output/tracking_results_rt_final/atom/default_000/0/
+
 trackers_fast_test =  trackerlist('atom', 'default') + \
             trackerlist('dimp', 'dimp18') + \
             trackerlist('kys', 'default')
