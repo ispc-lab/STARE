f4efde75cb686ac73e1aa4987ce41b0a0eab1942
Runze Zhang <101967641+NoRealBlank@users.noreply.github.com>
2025-07-07 13:10:12 +0800

    Update ostrack.py

--- lib/sotas/OSTrack/lib/test/parameter/ostrack.py
+++ lib/sotas/OSTrack/lib/test/parameter/ostrack.py
@@ -7,12 +7,14 @@ from lib.config.ostrack.config import cfg, update_config_from_file
 def parameters(yaml_name: str):
     params = TrackerParams()
     prj_dir = env_settings().prj_dir
-    save_dir = env_settings().save_dir
+    network_path = env_settings().network_path
+
     # update default config from yaml file
     yaml_file = os.path.join(prj_dir, 'experiments/ostrack/%s.yaml' % yaml_name)
     update_config_from_file(yaml_file)
     params.cfg = cfg
-    print("test config: ", cfg)
+
+    # print("test config: ", cfg)
 
     # template and search region
     params.template_factor = cfg.TEST.TEMPLATE_FACTOR
@@ -21,9 +23,9 @@ def parameters(yaml_name: str):
     params.search_size = cfg.TEST.SEARCH_SIZE
 
     # Network checkpoint path
-    params.checkpoint = os.path.join(save_dir, "checkpoints/train/ostrack/%s/OSTrack_ep%04d.pth.tar" %
+    params.checkpoint = os.path.join(network_path, "ostrack/%s/OSTrack_ep%04d.pth.tar" %
                                      (yaml_name, cfg.TEST.EPOCH))
-    # params.checkpoint = '/home/test4/code/OSTrack/output/checkpoints/train/ostrack/trial4_vitb_256_mae_ce_32x4_aligned/OSTrack_ep0300.pth.tar'
+
     # whether to save boxes from all queries
     params.save_all_boxes = False
 
