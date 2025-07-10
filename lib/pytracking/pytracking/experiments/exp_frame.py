2c237d0f038ef482622adeb8a21ea3be8aeeecb0
Runze Zhang <101967641+NoRealBlank@users.noreply.github.com>
2025-07-10 11:56:49 +0800

    Update exp_frame.py

--- lib/pytracking/pytracking/experiments/exp_frame.py
+++ lib/pytracking/pytracking/experiments/exp_frame.py
@@ -15,3 +15,31 @@ def fast_test_offline():
                 trackerlist('kys', 'default', range(1))
     dataset = get_dataset('esot_20_50')
     return trackers, dataset
+
+def esot500_frame_all():
+    trackers =  trackerlist('dimp', 'dimp18', range(1)) + \
+                trackerlist('dimp', 'dimp18_esot500', range(1)) + \
+                trackerlist('dimp', 'dimp50', range(1)) + \
+                trackerlist('dimp', 'dimp50_esot500', range(1)) + \
+                trackerlist('kys', 'default', range(1)) + \
+                trackerlist('kys', 'esot500', range(1)) + \
+                trackerlist('atom', 'default', range(1)) + \
+                trackerlist('atom', 'esot500', range(1)) + \
+                trackerlist('tomp', 'tomp50', range(1)) + \
+                trackerlist('tomp', 'tomp50_esot500', range(1)) + \
+                trackerlist('tomp', 'tomp101', range(1)) + \
+                trackerlist('tomp', 'tomp101_esot500', range(1)) + \
+                trackerlist('dimp', 'prdimp18', range(1)) + \
+                trackerlist('dimp', 'prdimp50', range(1)) + \
+                trackerlist('keep_track','default',range(1)) + \
+                trackerlist('rts', 'rts50', range(1)) + \
+                trackerlist('egt', 'egt', range(1))
+
+    dataset = get_dataset(
+        'esot_500_2','esot_250_2','esot_20_2',
+        'esot_500_50','esot_250_50','esot_20_50',
+        'esot_500_100','esot_250_100','esot_20_100',
+        'esot_500_150','esot_250_150','esot_20_150',
+    )
+
+    return trackers, dataset
