173f55212d954b2a225cef9ef2fbfbe44501d8b8
Runze Zhang <101967641+NoRealBlank@users.noreply.github.com>
2025-07-10 11:56:28 +0800

    Update exp_stare.py

--- lib/pytracking/pytracking/experiments/exp_stare.py
+++ lib/pytracking/pytracking/experiments/exp_stare.py
@@ -13,9 +13,35 @@ trackers_fast_test =  trackerlist('atom', 'default') + \
             trackerlist('dimp', 'dimp18') + \
             trackerlist('kys', 'default')
 
-def fast_test_streaming():
+def fast_test_stare():
     trackers = trackers_fast_test
     dataset = get_dataset('esot500s')
     stream_setting_id = 0  # Default streaming setting, for real-time testing on your own hardware.
     stream_setting = load_stream_setting(f's{stream_setting_id}')
+    return trackers, dataset, stream_setting
+
+def esot500_stare_all():
+    trackers =  trackerlist('dimp', 'dimp18', range(1)) + \
+                trackerlist('dimp', 'dimp18_esot500', range(1)) + \
+                trackerlist('dimp', 'dimp50', range(1)) + \
+                trackerlist('dimp', 'dimp50_esot500', range(1)) + \
+                trackerlist('kys', 'default', range(1)) + \
+                trackerlist('kys', 'esot500', range(1)) + \
+                trackerlist('atom', 'default', range(1)) + \
+                trackerlist('atom', 'esot500', range(1)) + \
+                trackerlist('tomp', 'tomp50', range(1)) + \
+                trackerlist('tomp', 'tomp50_esot500', range(1)) + \
+                trackerlist('tomp', 'tomp101', range(1)) + \
+                trackerlist('tomp', 'tomp101_esot500', range(1)) + \
+                trackerlist('dimp', 'prdimp18', range(1)) + \
+                trackerlist('dimp', 'prdimp50', range(1)) + \
+                trackerlist('keep_track','default',range(1)) + \
+                trackerlist('rts', 'rts50', range(1)) + \
+                trackerlist('egt', 'egt', range(1))
+
+    dataset = get_dataset('esot500s')
+    
+    stream_setting_id = 14  # for sim real-time testing on your own hardware.
+    stream_setting = load_stream_setting(f's{stream_setting_id}')
+
     return trackers, dataset, stream_setting
