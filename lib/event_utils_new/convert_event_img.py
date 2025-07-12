334c45ad0aa78ede36a0661d558d126411d7fe90
Runze Zhang <101967641+NoRealBlank@users.noreply.github.com>
2025-07-12 19:55:20 +0800

    Update convert_event_img.py

--- lib/event_utils_new/convert_event_img.py
+++ lib/event_utils_new/convert_event_img.py
@@ -2,33 +2,34 @@ import numpy as np
 import torch
 from event_utils_new import events_to_neg_pos_voxel_torch, events_to_voxel_torch
 
-def convert_event_img_aedat(events, style):
+def convert_event_img_aedat(events, style, height=260, width=346):
     """
     events: aedat4 events
     """
     if style == 'VisEvent': 
-        dvs_img = np.ones((260,346,3), dtype=np.uint8)*255
+        dvs_img = np.ones((height, width, 3), dtype=np.uint8) * 255
         dvs_img[events[:]['y'], events[:]['x']] = 1
         # dvs_img[events[:]['y'], events[:]['x']] = 255
         dvs_img[events[:]['y'], events[:]['x'], events[:]['polarity'] * 2] = 255
-    if style == 'FE240': 
-        dvs_img = np.ones((260,346,3), dtype=np.uint8)*0
+
+    elif style == 'FE240':
+        dvs_img = np.ones((height, width, 3), dtype=np.uint8) * 0
         dvs_img[events[:]['y'], events[:]['x']] = 1
         dvs_img[events[:]['y'], events[:]['x']] = 255
         dvs_img[events[:]['y'], events[:]['x'], events[:]['polarity'] * 2] = 255
 
     elif style == 'FE108old': 
-        dvs_img = torch.ones((260,346))*127
-        pos_mask = events[:]['polarity']==1
-        neg_mask = events[:]['polarity']==0
+        dvs_img = torch.ones((height, width)) * 127
+        pos_mask = events[:]['polarity'] == 1
+        neg_mask = events[:]['polarity'] == 0
         value=torch.ones(events.shape[0])
         y= events['y'].astype('int')
         x= events['x'].astype('int')
         y =  torch.Tensor(y).long()
         x =  torch.Tensor(x).long()
-        value[pos_mask]=255
-        value[neg_mask]=0
-        dvs_img.index_put_((y , x ),value,accumulate=False)
+        value[pos_mask] = 255
+        value[neg_mask] = 0
+        dvs_img.index_put_((y , x ), value, accumulate=False)
 
     elif style == 'VoxelGrid':
         device = torch.device('cuda')
@@ -37,10 +38,11 @@ def convert_event_img_aedat(events, style):
         ts = torch.from_numpy(events['timestamp']).to(device)
         ps = torch.from_numpy(events['polarity']).to(device)
         B = 3
-        sensor_size = (260,346)
+        # sensor_size = (260,346)
         temporal_bilinear = True
-        voxel_pos, voxel_neg = events_to_neg_pos_voxel_torch(xs, ys, ts, ps, B, device, sensor_size,temporal_bilinear)
-        voxel=torch.cat((voxel_pos,voxel_neg),dim=0)
+        voxel_pos, voxel_neg = events_to_neg_pos_voxel_torch(xs, ys, ts, ps, B, device, (height, width), temporal_bilinear)
+        voxel=torch.cat((voxel_pos, voxel_neg), dim=0)
+
         return voxel # (2*3, H , W)
 
     elif style == 'VoxelGridComplex':
@@ -49,25 +51,26 @@ def convert_event_img_aedat(events, style):
         ys = torch.from_numpy(events['y']).to(device)
         ts = torch.from_numpy(events['timestamp']).to(device)
         ps = torch.from_numpy(events['polarity']).to(device)
-        ps[ps==0] = -1 # for negative events
+        ps[ps == 0] = -1 # for negative events
         B = 3
-        sensor_size = (260,346)
+        # sensor_size = (260,346)
         temporal_bilinear = True
-        voxel_complex = events_to_voxel_torch(xs, ys, ts, ps, B, device, sensor_size,temporal_bilinear)
+        voxel_complex = events_to_voxel_torch(xs, ys, ts, ps, B, device, (height, width), temporal_bilinear)
+
         return voxel_complex # (3, H, W)
 
     elif style == 'TimeSurface':
         raise NotImplementedError
         """ pending"""
-        dvs_img = torch.zeros((260,346),dtype=np.uint8)
+        dvs_img = torch.zeros((height, width), dtype=np.uint8)
         left = events[0]['timestamp']
-        duration = events[-1]['timestamp']-events[0]['timestamp']
-        value = torch.from_numpy((events[:]['timestamp']-left)/duration*255).long()
+        duration = events[-1]['timestamp'] - events[0]['timestamp']
+        value = torch.from_numpy((events[:]['timestamp'] - left) / duration * 255).long()
         y= events['y'].astype('int')
         x= events['x'].astype('int')
         y =  torch.Tensor(y).long()
         x =  torch.Tensor(x).long()
-        dvs_img.index_put_((y , x ),value,accumulate=False)
+        dvs_img.index_put_((y, x), value, accumulate=False)
 
         raise NotImplemented
     else:
@@ -87,7 +90,7 @@ def convert_event_img_carla(events, resolution:tuple, style):
         B = 3
         sensor_size = resolution
         temporal_bilinear = True
-        voxel_complex = events_to_voxel_torch(xs, ys, ts, ps, B, device, sensor_size,temporal_bilinear)
+        voxel_complex = events_to_voxel_torch(xs, ys, ts, ps, B, device, sensor_size, temporal_bilinear)
         return voxel_complex # (3, H, W)
 
     else:
