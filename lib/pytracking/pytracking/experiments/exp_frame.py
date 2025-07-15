dbe1364a5da25a9abea8a64a3061b75f6380c955
Runze Zhang <101967641+NoRealBlank@users.noreply.github.com>
2025-07-15 16:00:34 +0800

    Update exp_frame.py

--- lib/pytracking/pytracking/experiments/exp_frame.py
+++ lib/pytracking/pytracking/experiments/exp_frame.py
@@ -31,15 +31,16 @@ def esot500_frame_all():
                 trackerlist('tomp', 'tomp101_esot500', range(1)) + \
                 trackerlist('dimp', 'prdimp18', range(1)) + \
                 trackerlist('dimp', 'prdimp50', range(1)) + \
-                trackerlist('keep_track','default',range(1)) + \
+                trackerlist('keep_track', 'default', range(1)) + \
                 trackerlist('rts', 'rts50', range(1)) + \
                 trackerlist('egt', 'egt', range(1))
 
     dataset = get_dataset(
-        'esot_500_2','esot_250_2','esot_20_2',
-        'esot_500_50','esot_250_50','esot_20_50',
-        'esot_500_100','esot_250_100','esot_20_100',
-        'esot_500_150','esot_250_150','esot_20_150',
+        'esot_500_2', 'esot_250_2', 'esot_20_2',
+        'esot_500_20', 'esot_250_20', 'esot_20_20',
+        'esot_500_50', 'esot_250_50', 'esot_20_50',
+        'esot_500_100', 'esot_250_100', 'esot_20_100',
+        'esot_500_150', 'esot_250_150', 'esot_20_150',
     )
 
     return trackers, dataset
